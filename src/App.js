252a06e9b3f6df51c46e728b51e0a9d0302534d2
Ywoosang <opellong13@gmail.com>
2022-05-29 05:18:15 +0900

    Add answer page navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Auth from './hoc/auth';
+// import Auth from './hoc/auth';
 
-import LandingPage from './pages/LandingPage/LandingPage';
-import LoginPage from './pages/LoginPage/LoginPage';
-import RegisterPage from './pages/RegisterPage/RegisterPage';
-import AdminPage from './pages/AdminPage/AdminPage';
-import { GuestbookPage } from './pages/GuestbookPage';
-import { QuestionPage } from './pages/QuestionPage' 
-
+import LandingPage from './components/LandingPage';
+import LoginPage from './components/Login/LoginPage';
+import RegisterPage from './components/Register/RegisterPage';
+import AdminPage from './components/Admin/AdminPage';
+import { GuestbookPage } from './components/Guestbook';
+import { QuestionPage } from './components/Question' 
+import { AnswerPage } from './components/Answer';
+import { CommentPage } from './components/Comment';
 // const AuthLandingPage = Auth(LandingPage, null);
 // const AuthLoginPage = Auth(LoginPage, false);
 // const AuthRegisterPage = Auth(RegisterPage, false);
@@ -23,6 +24,8 @@ function App() {
         <Route path="/admin" element={<AdminPage />} />
         <Route path="/guestbook" element={<GuestbookPage/>} />
         <Route path="/question" element={<QuestionPage/>} />
+        <Route path="/answer" element={<AnswerPage/>} />
+        <Route path="/answer/:answerId/comments" element={<CommentPage/>}/>
       </Routes>
     </Router>
   )
